e5f750f4233a3982d8cc96609cf746a9cb0f0bd7
atphinn <atphinn@gmail.com>
2019-12-20 17:37:26 -0500

    Breaking out projects, first BK Muzosa site

---
+++ src/components/header.js
@@ -0,0 +1,67 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `black`,
+      marginBottom: `1.45`,
+      paddingBottom:`10px`
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+
+      }}
+    >
+      <h1 style={{ margin: 0, }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+            padding: `0 20px 0 30px`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+
+        <Link 
+        id="typeContainer"
+          style={{
+          to:`/page-2/`,
+          color: `white`,
+          textDecoration: `none`,
+          padding: `0 20px 0 0`,
+          }}
+        >
+        About
+        </Link>
+       
+        <Link 
+        to="/contact/"
+          style={{
+          color: `white`,
+          textDecoration: `none`,
+          padding: `0 20px 0 0`,
+          }}
+        >
+        Contact
+        </Link>
+       
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
