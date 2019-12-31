2fd8daef75674327ee83fcb539f2838e51a8e3e7
atphinn <atphinn@gmail.com>
2019-12-31 11:18:35 -0500

    updated the index js design for the about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,9 +40,8 @@ const IndexPage = () => (
         {/* First image with ichimonji   */}
         <div id="about">
             <div id="typeContainer">
-                <h1 style={{margin: `55px 0 -1px 0px`}}>What is BK Muzosa Bujinkan</h1>
-                <p style={{width: `1000px`,float: `right`,margin: `-48px 200px 0 0px`}}>Brooklyn Muzosa Bujikan Dojo embraces the spirit of Budo - an armed and unarmed historical style of Martial Arts that pre dates modern practices such a Judo and Akido. Known as a living art, Bujinkan Budo Taijutsu is applicable within our modern culture.Training in the evenings in all weather, we believe the way of Budo, is to practice it in all situations and in all environments.
-                    <b>LOCATION:Training takes place from 7:00 pm-9:00 pm on the porch and surrounding grounds of Litchfield Villa in Prospect Park </b></p>
+                <h1 style={{margin: `55px 0 0px 0px`}}>What is BK Muzosa Bujinkan</h1>
+                <p style={{width: `681px`,float: `right`,margin: `-48px 0px 48px 0px`}}>Brooklyn Muzosa Bujikan Dojo embraces the spirit of Budo - an armed and unarmed historical style of Martial Arts that pre dates modern practices such a Judo and Akido. Known as a living art, Bujinkan Budo Taijutsu is applicable within our modern culture.Training in the evenings in all weather, we believe the way of Budo, is to practice it in all situations and in all environments.</p>
             </div>
           <img src={require('../images/aboutTop.jpg')} alt="Punch and evasion" style={{width: `100%`}}/>
           {/*<div id="typeContainer">*/}
@@ -55,13 +54,12 @@ const IndexPage = () => (
 
         <div id="seperate"></div>
          {/* about the bujinkan  */}
-        <div id="aboutBujinkan" style={{ marginTop: '250px' }}>
-            <img src={require('../images/masaaki-hatsumi.jpg')} alt="Soke" id="soke" />
-            <div id="typeContainer2">
-                <h1 style={{}}>History of the Bujinkan</h1>
+        <div id="aboutBujinkan" style={{  }}>
+        <div id="typeContainer">
+                <h1 style={{margin: `55px 0 0px 0px`}}>History of the Bujinkan</h1>
 
 
-                <p>The Bujinkan Dojo was organized in the early 1970's by Masaaki Hatsumi. This organization is comprised of nine distinct schools of ancient Japanese martial arts, with traceable histories from the 9th century and earlier. Hatsumi-sensei inherited these ryuha from his teacher, the late Toshitsugu Takamatsu, in 1972.The Bujinkan Budo Taijutsu system of martial arts emphasizes natural and relaxed body movement. This martial art is comprised of authentic techniques from ancient samurai and ninja combat arts, taught within both their historical contexts as well as within the modern millieu.
+                <p style={{width: `681px`,float: `right`,margin: `-48px 0px 48px 0px`}}>The Bujinkan Dojo was organized in the early 1970's by Masaaki Hatsumi. This organization is comprised of nine distinct schools of ancient Japanese martial arts, with traceable histories from the 9th century and earlier. Hatsumi-sensei inherited these ryuha from his teacher, the late Toshitsugu Takamatsu, in 1972.The Bujinkan Budo Taijutsu system of martial arts emphasizes natural and relaxed body movement. This martial art is comprised of authentic techniques from ancient samurai and ninja combat arts, taught within both their historical contexts as well as within the modern millieu.
 
                     The subtle destruction of the attacker's balance and rhythm and proper control of distance and timing are a primary focus of Bujinkan Budo Taijutsu. These principles allow even smaller persons to overcome larger and more powerful opponents.
                     <a href="https://www.muzosa.com/articles" target="_blank" >
@@ -71,29 +69,30 @@ const IndexPage = () => (
                     </a>
                 </p>
             </div>
+            <img src={require('../images/masaaki-hatsumi.jpg')} alt="Soke" id="soke" className="img-responsive"/>
         </div>
         {/* about the bujinkan  */}
 
         <div id="seperate"></div>
 
-        {/* Map  */}
-        <div id='map'>
-            <MyMapComponent isMarkerShown/>
-        </div>
-        {/* Map */}
-
         {/* Contact Info  */}
         <div id="contactBody">
-            <div id="contactInfo">
+            <div id="contactInfo" style={{textAlign: `center`}}>
                 <a href="https://www.muzosa.com/contact" target="_blank" >
                     <h1>Contact us to try a free class!</h1>
                 </a>
                 <p>LOCATION:Training takes place from 7:00 pm-9:00 pm at Dance Wave studios 182 4th Ave - Degraw Street</p>
-                <p>Take the R train to Union street and walk to Degraw along 4th ave</p>
-                <img src={require('../images/dancewave.png')} alt="Litchfield villa" className="img-responsive" />
+                <p>Take the R train to Union street and walk to Degraw along 4th ave</p> 
             </div>
+            <img src={require('../images/dancewave.png')} alt="Litchfield villa" className="img-responsive" />
         </div>
+         
         {/* Contact Info  */}
+                {/* Map  */}
+                <div id='map'>
+            <MyMapComponent isMarkerShown/>
+        </div>
+        {/* Map */}
     </div>
     
   </Layout>
